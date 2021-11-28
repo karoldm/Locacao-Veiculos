187ec08414b18a7c9c84858881a328cb72652820
karolyne <karoldm1579@gmail.com>
2021-11-28 15:47:59 -0300

    create controller

---
+++ src/Controller/Controller.java
@@ -0,0 +1,22 @@
+
+package Controller;
+
+import Models.Locacao;
+import Models.Seguro;
+import Models.Usuario;
+import Models.Veiculo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author karolyne Marques
+ */
+public class Controller {
+    private ArrayList<Locacao> locacoes;
+    private ArrayList<Veiculo> veiculos;
+    private ArrayList<Usuario> usuarios;
+    private ArrayList<Seguro> seguros;
+    //private Configuracao configuracao;
+    
+    
+}
